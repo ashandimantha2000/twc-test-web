d4ea791857acbcca81c2a0b0663f37c64eb4682f
Ashan Dimantha <ashandimanthalk@gmail.com>
2024-04-16 23:07:11 +0530

    fix: handle errors

--- src/components/micro-components/Button.jsx
+++ src/components/micro-components/Button.jsx
@@ -1,22 +1,29 @@
-import PropTypes from 'prop-types';
-import { Link } from 'react-router-dom';
+import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
 
 function Button(props) {
-    return (
-        <Link to={props.destination}><button className="h-fit w-fit p-0 bg-transparent hover:bg-white text-white font-semibold hover:text-primary-green py-2 px-4 border border-white hover:border-transparent rounded-full">
+  return (
+    <Link to={props.destination}>
+      <button
+        onClick={props.onClick}
+        className="h-fit w-fit p-0 bg-transparent hover:bg-white text-white font-semibold hover:text-primary-green py-2 px-4 border border-white hover:border-transparent rounded-full"
+      >
         {props.text}
-    </button></Link>
-    );
+      </button>
+    </Link>
+  );
 }
 
 Button.propTypes = {
-    text: PropTypes.string.isRequired,
+  text: PropTypes.string.isRequired,
 };
-
 
 Button.propTypes = {
-    destination: PropTypes.string.isRequired,
+  destination: PropTypes.string.isRequired,
 };
 
+Button.propTypes = {
+  onClick: PropTypes.func,
+};
 
 export default Button;
