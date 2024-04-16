990105a156c79d77de111749fe0bc0695224963f
Ashan Dimantha <ashandimanthalk@gmail.com>
2024-04-16 13:21:19 +0530

    add: login

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
-// import Layout from "./components/layout.jsx"
+import { Routes, Route } from "react-router-dom";
+
 import Login from "./components/Login.jsx"
 import Register from "./components/Register.jsx"
+import Welcome from "./components/Welcome.jsx"
 
 function App() {
   return (
-    <div>
-      {/* <Login /> */}
-      <Register />
-    </div>
+    <Routes>
+      <Route path="/" element={<Welcome />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
+    </Routes>
   )
 }
 
