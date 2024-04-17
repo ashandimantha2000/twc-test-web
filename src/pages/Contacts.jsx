9cff961412fcca5d172b81ab8e5b5893cc428852
Ashan Dimantha <ashandimanthalk@gmail.com>
2024-04-17 23:23:53 +0530

    add: delete operation

--- src/pages/Contacts.jsx
+++ src/pages/Contacts.jsx
@@ -12,6 +12,7 @@ function Contacts() {
   const [contacts, setContacts] = useState([]);
   const [loading, setLoading] = useState(true);
   const [showNotification, setShowNotification] = useState(false);
+  const [selectedId, setSelectedId] = useState(null); // Add selectedId state variable
 
   useEffect(() => {
     setLoading(true);
@@ -29,55 +30,66 @@ function Contacts() {
 
   return (
     <div>
-      <DeleteNotification trigger={showNotification} setClose={setShowNotification} text="Yes" secondary="Cancel">
+      <DeleteNotification
+        id={selectedId}
+        trigger={showNotification}
+        setClose={setShowNotification}
+        text="Yes"
+        secondary="Cancel"
+      >
         <h1>Do you want to delete the contact?</h1>
       </DeleteNotification>
-      
+
       <Background>
-      <div className="flex justify-between pt-12">
-        <h2 className="title">Contacts</h2>
-        <Button text="add new contact" destination="/contacts/new" />
-      </div>
-      <br />
-      {loading ? (
-        <Spinner />
-      ) : (
-        <div className="bg-white relative overflow-x-auto shadow-md sm:rounded-lg">
-          <table className="w-full text-sm text-left">
-            <thead>
-              <tr>
-                <th className="px-6 py-3"></th>
-                <th className="px-6 py-3">full name</th>
-                <th className="px-6 py-3">gender</th>
-                <th className="px-6 py-3">e-mail</th>
-                <th className="px-6 py-3">phone</th>
-                <th className="px-6 py-3"></th>
-              </tr>
-            </thead>
-            <tbody>
-              {contacts.map((contact) => (
-                <tr key={contact._id}>
-                  <th className="px-6 py-4 font-medium ">
-                    <img src="wwf" alt="image" />
-                  </th>
-                  <td className="px-6 py-4">{contact.fullname}</td>
-                  <td className="px-6 py-4">{contact.gender}</td>
-                  <td className="px-6 py-4">{contact.email}</td>
-                  <td className="px-6 py-4">{contact.phonenumber}</td>
-                  <td className="px-6 py-4 flex justify-around">
-                    <MdModeEdit />
-                    <Link to={`/contacts/delete/${contact._id}`}>
-                      <RiDeleteBinLine />
-                    </Link>
-                    <button onClick={()=>setShowNotification(true)}>Toast</button>
-                  </td>
-                </tr>
-              ))}
-            </tbody>
-          </table>
+        <div className="flex justify-between pt-12">
+          <h2 className="title">Contacts</h2>
+          <Button text="add new contact" destination="/contacts/new" />
         </div>
-      )}
-    </Background>
+        <br />
+        {loading ? (
+          <Spinner />
+        ) : (
+          <div className="bg-white relative overflow-x-auto shadow-md sm:rounded-lg">
+            <table className="w-full text-sm text-left">
+              <thead>
+                <tr>
+                  <th className="px-6 py-3"></th>
+                  <th className="px-6 py-3">full name</th>
+                  <th className="px-6 py-3">gender</th>
+                  <th className="px-6 py-3">e-mail</th>
+                  <th className="px-6 py-3">phone</th>
+                  <th className="px-6 py-3"></th>
+                </tr>
+              </thead>
+              <tbody>
+                {contacts.map((contact) => (
+                  <tr key={contact._id}>
+                    <th className="px-6 py-4 font-medium ">
+                      <img src="wwf" alt="image" />
+                    </th>
+                    <td className="px-6 py-4">{contact.fullname}</td>
+                    <td className="px-6 py-4">{contact.gender}</td>
+                    <td className="px-6 py-4">{contact.email}</td>
+                    <td className="px-6 py-4">{contact.phonenumber}</td>
+                    <td className="px-6 py-4 flex justify-around">
+                      <MdModeEdit />
+
+                      <button
+                        onClick={() => {
+                          setSelectedId(contact._id);
+                          setShowNotification(true);
+                        }}
+                      >
+                        <RiDeleteBinLine />
+                      </button>
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
+        )}
+      </Background>
     </div>
   );
 }
